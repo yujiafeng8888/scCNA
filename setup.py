f10df66c92d6ae4973d13730feb62cef8f5f4426
Yujia Feng <yfeng80@jh.edu>
2025-05-03 09:19:15 -0400

    change setup

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="scCNA",  # Name of the package
-    version="0.1",  # Package version
+    name="scCNA",
+    version="0.1",
     description="A package for detecting and annotating Copy Number Aberrations in single-cell RNA-seq data",
-    author="Your Name",  # Replace with your name
-    author_email="your.email@example.com",  # Replace with your email
-    url="https://github.com/yujiafeng8888/scCNA",  # Replace with your project URL
-    packages=find_packages(),  # Automatically find packages in the project
+    author="Your Name",
+    author_email="your.email@example.com",
+    url="https://github.com/yujiafeng8888/scCNA",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},  
     install_requires=[
         "pandas",
         "numpy",
@@ -19,15 +20,10 @@ setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.10",  # Minimum Python version required
+    python_requires=">=3.10",
     include_package_data=True,
     zip_safe=False,
 )
